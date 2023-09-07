9221beb24c1a7b82b5bed37273ca656694949e88
Tarun <ttarunn0709@gmail.com>
2023-09-07 21:10:45 +0530

    Add todo app in git repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [todo, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  function addTodo() {
+    if (input === "") {
+      alert("Add valid todo");
+    } else {
+      setTodos([...todo, input]);
+      setInput("");
+    }
+  }
+
+  function deleteTodo(item) {
+    setTodos(todo.filter((data) => data !== item));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main-container">
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <button onClick={addTodo} id="add-todo">Add Todo</button>
+      <div>
+        <ul>
+          {todo.length > 0
+            ? todo.map((item, i) => (
+                <li key={i}>
+                  {item}{" "}
+                  <button onClick={() => deleteTodo(item)}>Delete</button>
+                </li>
+              ))
+            : ""}
+        </ul>
+      </div>
     </div>
   );
 }
